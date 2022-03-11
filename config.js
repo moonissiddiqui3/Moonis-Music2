3afe104c4fc3287ddcbfa2e98648d497f116a862
moonissiddiqui3 <101393126+moonissiddiqui3@users.noreply.github.com>
2022-03-11 15:31:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
     app: {
         px: 'XXX',
-        token: 'XXX',
-        playing: 'by Zerio ❤️'
+        token: 'OTUxNzc2NjMwODQ0NTcxNjg4.YisZDw.kplf8M3hUg03X54oshjQqPVgHPI',
+        playing: 'Custom Bot by ❤️ MOONIS'
     },
 
     opt: {
         DJ: {
             enabled: false,
             roleName: 'DJ',
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume' ,'24/7']
         },
         maxVol: 100,
         loopMessage: false,
